89d52b8925cb20e5aad4782d7cb24ad72773e0b0
Christian Rauch <Christian.Rauch@ed.ac.uk>
2020-08-25 16:21:02 +0100

    build library cache for core ROS workspace

--- set_caps.sh
+++ set_caps.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# make sure all libraries in the core ROS workspace are cached
+ros_lib_path=$(catkin_find --lib)
+sudo ldconfig $ros_lib_path
+
 # get full path to youbot_driver_ros_interface executable
 exec_path=$(readlink -f $(catkin_find --first-only youbot_driver_ros_interface/youbot_driver_ros_interface))
 sudo setcap cap_net_raw+ep $exec_path
