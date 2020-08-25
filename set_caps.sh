bd894327f8eb412b46e4855487d335616f0fb6d3
Christian Rauch <Christian.Rauch@ed.ac.uk>
2020-08-25 16:07:15 +0100

    determine full path to 'youbot_driver_ros_interface' via catkin

--- set_caps.sh
+++ set_caps.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo setcap cap_net_raw+ep /home/youbot/youbot_ws/install/lib/youbot_driver_ros_interface/youbot_driver_ros_interface
-sudo ldconfig /home/youbot/youbot_ws/install/lib/youbot_driver_ros_interface/youbot_driver_ros_interface
+# get full path to youbot_driver_ros_interface executable
+exec_path=$(readlink -f $(catkin_find --first-only youbot_driver_ros_interface/youbot_driver_ros_interface))
+sudo setcap cap_net_raw+ep $exec_path
+sudo ldconfig $exec_path
